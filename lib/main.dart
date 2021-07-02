f43117fbdee430b1af15dce609861a4394195f33
FlutLab.io <>
2021-07-02 03:17:20 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -244,7 +244,7 @@ class _HomePageState extends State<HomePage> {
                           )
                         : Expanded(
                             child: ListView(
-                              children: _tors.map((tor) {
+                              children: _tors['results'].map((tor) {
                                 return ListTile(
                                     title: Text(
                                       tor['title'],
