2d81fcc0def8b55b4b71d2b7e32678c4fbd7c981
Giridhar Salana <giridharsalana@gmail.com>
2022-03-10 16:03:35 +0000

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ final dio = Dio(
 );
 String query = "";
 List Tors = [];
+bool isSearching = false;
 
 // This widget is the root of your application.
 class MyApp extends StatelessWidget {
@@ -57,6 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
       query = '';
       controller.clear();
       Tors = [];
+      isSearching = false;
       FocusManager.instance.primaryFocus?.unfocus();
     });
   }
@@ -87,116 +89,119 @@ class _MyHomePageState extends State<MyHomePage> {
               icon: const Icon(Icons.account_circle),
               onPressed: () {
                 showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return Dialog(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.0)),
-                        child: Container(
-                          height: 350,
-                          width: 280,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Developer',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontFamily: 'Pacifico',
-                                    fontSize: 25.0,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Dialog(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Container(
+                        height: 350,
+                        width: 280,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Developer',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Pacifico',
+                                  fontSize: 25.0,
+                                ),
+                              ),
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(15.0),
+                                child: Image.asset(
+                                  "assets/images/Giri.jpg",
+                                  width: 180,
+                                  height: 200,
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Text(
+                                'Giridhar Salana',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontFamily: 'Pacifico',
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                      icon: Icon(FontAwesomeIcons.facebook,
+                                          color: Colors.red),
+                                      onPressed: () async {
+                                        const fburl =
+                                            'https://www.facebook.com/GiridharSalana';
+                                        if (await canLaunch(fburl)) {
+                                          await launch(fburl);
+                                        } else {
+                                          throw 'Could not launch $fburl';
+                                        }
+                                      }),
+                                  SizedBox(width: 20),
+                                  IconButton(
+                                      icon: Icon(FontAwesomeIcons.linkedin,
+                                          color: Colors.red),
+                                      onPressed: () async {
+                                        const liurl =
+                                            'https://www.linkedin.com/in/Giridhar-Salana';
+                                        if (await canLaunch(liurl)) {
+                                          await launch(liurl);
+                                        } else {
+                                          throw 'Could not launch $liurl';
+                                        }
+                                      }),
+                                  SizedBox(width: 20),
+                                  IconButton(
+                                      icon: Icon(FontAwesomeIcons.github,
+                                          color: Colors.red),
+                                      onPressed: () async {
+                                        const giurl =
+                                            'https://github.com/Giridharsalana';
+                                        if (await canLaunch(giurl)) {
+                                          await launch(giurl);
+                                        } else {
+                                          throw 'Could not launch $giurl';
+                                        }
+                                      }),
+                                  SizedBox(width: 20),
+                                  IconButton(
+                                    icon: Icon(FontAwesomeIcons.twitter,
+                                        color: Colors.red),
+                                    onPressed: () async {
+                                      const tiurl =
+                                          'https://www.twitter.com/GiridharSalana3';
+                                      if (await canLaunch(tiurl)) {
+                                        await launch(tiurl);
+                                      } else {
+                                        throw 'Could not launch $tiurl';
+                                      }
+                                    },
                                   ),
-                                ),
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(15.0),
-                                  child: Image.asset(
-                                    "assets/images/Giri.jpg",
-                                    width: 180,
-                                    height: 200,
-                                    fit: BoxFit.fill,
-                                  ),
-                                ),
-                                Text(
-                                  'Giridhar Salana',
-                                  style: TextStyle(
-                                    color: Colors.green,
-                                    fontFamily: 'Pacifico',
-                                    fontSize: 20.0,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      IconButton(
-                                          icon: Icon(FontAwesomeIcons.facebook,
-                                              color: Colors.red),
-                                          onPressed: () async {
-                                            const fburl =
-                                                'https://www.facebook.com/GiridharSalana';
-                                            if (await canLaunch(fburl)) {
-                                              await launch(fburl);
-                                            } else {
-                                              throw 'Could not launch $fburl';
-                                            }
-                                          }),
-                                      SizedBox(width: 20),
-                                      IconButton(
-                                          icon: Icon(FontAwesomeIcons.linkedin,
-                                              color: Colors.red),
-                                          onPressed: () async {
-                                            const liurl =
-                                                'https://www.linkedin.com/in/Giridhar-Salana';
-                                            if (await canLaunch(liurl)) {
-                                              await launch(liurl);
-                                            } else {
-                                              throw 'Could not launch $liurl';
-                                            }
-                                          }),
-                                      SizedBox(width: 20),
-                                      IconButton(
-                                          icon: Icon(FontAwesomeIcons.github,
-                                              color: Colors.red),
-                                          onPressed: () async {
-                                            const giurl =
-                                                'https://github.com/Giridharsalana';
-                                            if (await canLaunch(giurl)) {
-                                              await launch(giurl);
-                                            } else {
-                                              throw 'Could not launch $giurl';
-                                            }
-                                          }),
-                                      SizedBox(width: 20),
-                                      IconButton(
-                                          icon: Icon(FontAwesomeIcons.twitter,
-                                              color: Colors.red),
-                                          onPressed: () async {
-                                            const tiurl =
-                                                'https://www.twitter.com/GiridharSalana3';
-                                            if (await canLaunch(tiurl)) {
-                                              await launch(tiurl);
-                                            } else {
-                                              throw 'Could not launch $tiurl';
-                                            }
-                                          }),
-                                    ])
-                              ],
-                            ),
+                                ],
+                              )
+                            ],
                           ),
                         ),
-                      );
-                    });
+                      ),
+                    );
+                  },
+                );
               },
             );
           }),
         ],
       ),
       body: Column(
-        children: [
+        children: <Widget>[
           Searchbar(onClick: UpdateState),
           Expanded(
             child: Resultsbar(),
@@ -214,7 +219,8 @@ class Searchbar extends StatefulWidget {
   final Function onClick;
   void SearchTorrents(String query, Function onClick) async {
     Tors = [];
-    // print("Number of Torrents Bf : ${Tors.length}");
+    isSearching = true;
+    onClick();
     try {
       final response = await dio.get('', queryParameters: {
         'query': query,
@@ -226,6 +232,7 @@ class Searchbar extends StatefulWidget {
     } catch (e) {
       print(e);
     }
+    isSearching = false;
     onClick();
   }
 
@@ -302,12 +309,14 @@ class _Searchbar extends State<Searchbar> {
                       ),
                       tooltip: "Search",
                       onPressed: () {
-                        setState(() {
-                          query = controller.text;
-                          widget.onClick();
-                          FocusManager.instance.primaryFocus?.unfocus();
-                          widget.SearchTorrents(query, widget.onClick);
-                        });
+                        setState(
+                          () {
+                            query = controller.text;
+                            widget.onClick();
+                            FocusManager.instance.primaryFocus?.unfocus();
+                            widget.SearchTorrents(query, widget.onClick);
+                          },
+                        );
                       },
                     ),
                   ],
@@ -354,7 +363,7 @@ class _Resultsbar extends State<Resultsbar> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Expanded(
+        !isSearching ? Expanded(
           child: ListView.builder(
             itemCount: Tors.length,
             itemBuilder: (context, index) {
@@ -407,7 +416,8 @@ class _Resultsbar extends State<Resultsbar> {
               );
             },
           ),
-        ),
+        ) 
+        : CircularProgressIndicator(),
       ],
     );
   }
